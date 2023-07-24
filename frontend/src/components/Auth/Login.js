3331ba5e4897cadc25901a19c6a1850e9668a830
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-24 12:03:18 +0200

    fix: format login page and refactor validate

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,110 +1,98 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import './Login.css'
 
-import './Login.css'; 
+import logo from '../../logo.svg'
+import axios from 'axios'
 
-import logo from "../../logo.svg";
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { envLoader } from '../../env/envLoader';
+import tokenService from '../../services/tokenService'
 
-const apiUrl = "http://localhost:8080/api";
+const apiUrl = process.env.REACT_APP_API_URL
 
 function Login() {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  })
 
+  const [error, setError] = useState(false)
 
-    const [form,setForm] = useState({
-      "username":"",
-      "password":""
-    })
+  const [errorMsg, setErrorMsg] = useState(false)
+  const navigate = useNavigate()
 
-    const [error,setError] = useState(false);
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
-    const [errorMsg,setErrorMsg] = useState(false);
-
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    }
-
-
-    const handleChange = async e =>{
-      setForm({...form, [e.target.name]: e.target.value});
-
-    }
-
-    const handleButton = () => {
-      let url = apiUrl + "/login";
-      if (validate(form)){
-        setError(false);
-        axios.post(url,form)
-        .then( response =>{
-          
-          if(response.status === 200){
-            
-            localStorage.setItem("jwtToken",response.data);
-            window.location.replace('/games/list');
-            
+  const handleSubmit = e => {
+    e.preventDefault()
+    const url = apiUrl + '/login'
+    if (validate(form)) {
+      axios
+        .post(url, form)
+        .then(response => {
+          if (response.status === 200) {
+            tokenService.updateLocalAccessToken(response.data)
+            navigate('/games/list')
           }
-        
-        }).catch(error => {
-          setError(true);
-          setErrorMsg("Credenciales Incorrectas");
-        });
-
-      }else{
-        setError(true);
-        setErrorMsg("La longitud de los campos debe ser mayor que 1");
-      }
-      
-    }
-      
-    return(
-        
-       <React.Fragment>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-            <div className="wrapper fadeInDown">
-              <div id="formContent">
-              
-                <div className="fadeIn first">
-                  <img src={logo} width="100px" alt="User Icon" />
-                </div>
-
-                <form onSubmit={handleSubmit}>
-                  <input type="text"  className="fadeIn second" name="username" placeholder="Usuario" required onChange={handleChange}/>
-                  <input type="password" className="fadeIn third" name="password" placeholder="Contraseña" required onChange={handleChange}/>
-                  <input type="submit" className="fadeIn fourth" value="Iniciar Sesion" onClick={handleButton}/>
-                </form>
-
-            {error === true &&
-              <div className="alert alert-danger" role = "alert">
-                {errorMsg}
-              </div>
-            }
-            </div>
-            </div>
-      
-      </React.Fragment>
-
-
-
-    )
-}
-
-
-function validate(props){
-  if (props["username"].length > 1 && props["password"].length > 1){
-    return true;
-    }else{
-      return false;
+        })
+        .catch(error => {
+          setError(true)
+          setErrorMsg('Credenciales Incorrectas')
+          console.error(error)
+        })
+    } else {
+      setError(true)
+      setErrorMsg('La longitud de los campos debe ser mayor que 1')
     }
   }
 
+  return (
+    <div className='wrapper fadeInDown'>
+      <div id='formContent'>
+        <div className='fadeIn first'>
+          <img src={logo} width='100px' alt='User Icon' />
+        </div>
 
-export default Login;
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            className='fadeIn second'
+            name='username'
+            placeholder='Usuario'
+            required
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            className='fadeIn third'
+            name='password'
+            placeholder='Contraseña'
+            required
+            onChange={handleChange}
+          />
+          <input
+            type='submit'
+            className='fadeIn fourth'
+            value='Iniciar Sesion'
+          />
+        </form>
 
+        {error === true && (
+          <div className='alert alert-danger' role='alert'>
+            {errorMsg}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function validate(props) {
+  return props.username.length > 1 && props.password.length > 1
+}
+
+export default Login
