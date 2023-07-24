96719a5fad4bc7224c55f21be895c068ae9f2f65
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-24 12:07:19 +0200

    fix: format AwaitGame.js

--- frontend/src/components/Games/AwaitGame.js
+++ frontend/src/components/Games/AwaitGame.js
@@ -1,64 +1,47 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react'
 
-import axios from 'axios';
+import axios from 'axios'
 
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
 
-import { envLoader } from '../../env/envLoader';
+import NavBar from '../../Navbar'
+import tokenService from '../../services/tokenService'
 
-
-import NavBar from '../../Navbar';
-
-const apiUrl = "http://localhost:8080/api";
+const apiUrl = process.env.REACT_APP_API_URL
 
 function AwaitGame() {
+  useEffect(() => {
+    setInterval(numberOfPlayers, 1000)
+  }, [])
 
-   useEffect(() =>{
-    setInterval(numberOfPlayers,1000);
-   },[])
- 
-    
-   const sampleLocation = useLocation();
-   
-   
-    const numberOfPlayers = () =>{
-            
-            const token = localStorage.getItem("jwtToken");
-    
-            let url = apiUrl + sampleLocation.pathname;
-        
-            
-            axios.get(url,
-                {
-                    headers: {
-                    "Authorization": `Bearer  ${token}`
-                    }
-    
-                }).then( response =>{
-                    if(response.data===2){
-                        const direccion = sampleLocation.pathname;
-                        window.location.replace(direccion.substring(0,direccion.lastIndexOf("/")));
-                    }
-                    
-    
-                    }
-    
-                    )
-            }
- 
+  const sampleLocation = useLocation()
 
-    
+  const numberOfPlayers = () => {
+    const token = tokenService.getLocalAccessToken()
+    const url = apiUrl + sampleLocation.pathname
 
-    return(
-        <React.Fragment>
-            <NavBar></NavBar>
-        <div>
-            <h1>Esperando a que se una otro jugador</h1>
-        </div>
-        </React.Fragment>
-        )
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then(response => {
+        if (response.data === 2) {
+          const direccion = sampleLocation.pathname
+          window.location.replace(
+            direccion.substring(0, direccion.lastIndexOf('/')),
+          )
+        }
+      })
+  }
 
+  return (
+    <>
+      <NavBar></NavBar>
+      <h1>Esperando a que se una otro jugador</h1>
+    </>
+  )
 }
 
-
-export default AwaitGame;
+export default AwaitGame
