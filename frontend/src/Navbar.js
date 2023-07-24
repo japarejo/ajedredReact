cce42cf4fdd77ae7f2259693a4e30bdbf1190ecf
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-24 11:57:08 +0200

    feat: format navbar and fix UI disappearance

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
+import { Outlet, NavLink } from 'react-router-dom'
 
-import { Navbar, Container, Nav  } from "react-bootstrap"
+import './Navbar.css'
 
-import { Outlet, Link, NavLink } from "react-router-dom"
-
-import './Navbar.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function NavBar() {
+  return (
+    <>
+      <Navbar variant='light' expand={true}>
+        <Container>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <NavLink
+                to='/games/create'
+                className='nav-link'
+                activeclassname='active'>
+                Crear Partida
+              </NavLink>
+              <NavLink
+                to='/games/list'
+                className='nav-link'
+                activeclassname='active'>
+                Partidas Disponibles
+              </NavLink>
+              <NavLink
+                to='/player'
+                className='nav-link'
+                activeclassname='active'>
+                Datos Personales
+              </NavLink>
 
-        return(
-            <>    
-            <Navbar className="navbar" variant="white" expand="lg">
-             <Container>
-                 <Navbar.Collapse id="basic-navbar-nav">
+              <NavLink to='/logout' className='nav-link'>
+                Cerrar Sesion
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-                 <Nav className="ml-auto">
-
-                    <NavLink to="/games/create" className="nav-link" activeclassname="active">
-                        Crear Partida
-                    </NavLink>
-                    <NavLink to="/games/list" className="nav-link" activeclassname="active">
-                        Partidas Disponibles
-                    </NavLink>
-                    <NavLink to="/player" className="nav-link" activeclassname="active">
-                        Datos Personales
-                    </NavLink>
-
-                    <NavLink to="/logout" className="nav-link">
-                        Cerrar Sesion
-                    </NavLink>
-                </Nav>         
-                
-                 </Navbar.Collapse>
-             </Container>
-             </Navbar>  
-     
-             <section>
-                 <Outlet></Outlet>
-             </section> 
-            </> 
-            )
+      <section>
+        <Outlet></Outlet>
+      </section>
+    </>
+  )
 }
 
-
-export default NavBar;
+export default NavBar
