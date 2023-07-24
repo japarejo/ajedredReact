5cf8ff1808fb36fde366b9843356e3d00b9e1425
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-24 12:13:55 +0200

    fix: formatting files

--- frontend/src/components/Games/ListGame.js
+++ frontend/src/components/Games/ListGame.js
@@ -1,160 +1,150 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import axios from 'axios';
+import axios from 'axios'
 
-import NavBar from '../../Navbar';
+import NavBar from '../../Navbar'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { envLoader } from '../../env/envLoader';
+import Cookies from 'js-cookie'
 
-import Cookies from 'js-cookie';
+import Button from 'react-bootstrap/Button'
+import tokenService from '../../services/tokenService'
 
-
-import { Button } from 'reactstrap';
-
-const apiUrl = "http://localhost:8080/api";
+const apiUrl = process.env.REACT_APP_API_URL
 
 function ListGame() {
+  const [games, setGames] = useState([])
 
-    const[games,setGames] = useState([]);
+  useEffect(() => {
+    listGames()
+  }, [])
 
-    
-  
+  const unirsePartida = (id, tiempo) => {
+    const token = tokenService.getLocalAccessStorage()
 
-    useEffect(() => {
-       listGames()
-       setInterval(listGames,500);
-        
-    },[])
+    const url = apiUrl + '/games/' + id + '/join'
 
-    
-    const unirsePartida = (id,tiempo) => {
-        const token = localStorage.getItem("jwtToken");
-
-        let url = apiUrl + "/games/" + id + "/join";
-      
-       
-        axios.get(url,
-            {
-                headers: {
-                "Authorization": `Bearer  ${token}`
-                }
-
-            }).then( response =>{
-                if(response.data==='OK'){
-                    Cookies.set("time",tiempo * 60);
-                    Cookies.set("timeOpponent",tiempo*60);
-                    window.location.replace("/games/" + id);
-                }
-                })
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then(response => {
+        if (response.data === 'OK') {
+          Cookies.set('time', tiempo * 60)
+          Cookies.set('timeOpponent', tiempo * 60)
+          window.location.replace('/games/' + id)
         }
+      })
+  }
 
-    
-    const espectador = (id,tiempo) => {
-        const token = localStorage.getItem("jwtToken");
-    
-        let url = apiUrl + "/games/" + id + "/awaitGame";
-          
-           
-        axios.get(url,{headers: {"Authorization": `Bearer  ${token}`}})
-            .then( response =>{
-                
-                if(response.data===2){
-                    let url = apiUrl + "/games/" + id + "/espectador";
+  const espectador = (id, tiempo) => {
+    const token = tokenService.getLocalAccessStorage()
 
-                    axios.get(url,{headers: {"Authorization": `Bearer  ${token}`}})
-                    .then( response =>{
+    const url = apiUrl + '/games/' + id + '/awaitGame'
 
-                        Cookies.set("time",response.data[0]);
-                        Cookies.set("timeOpponent", response.data[1]);
+    axios
+      .get(url, { headers: { Authorization: `Bearer  ${token}` } })
+      .then(response => {
+        if (response.data === 2) {
+          const url = apiUrl + '/games/' + id + '/espectador'
 
-                        window.location.replace("/games/" + id);
+          axios
+            .get(url, { headers: { Authorization: `Bearer  ${token}` } })
+            .then(response => {
+              Cookies.set('time', response.data[0])
+              Cookies.set('timeOpponent', response.data[1])
 
-                    })
-                }else{
+              window.location.replace('/games/' + id)
+            })
+        } else {
+          Cookies.set('time', tiempo * 60)
+          Cookies.set('timeOpponent', tiempo * 60)
 
-                    Cookies.set("time",tiempo * 60);
-                    Cookies.set("timeOpponent",tiempo*60);
-
-                    window.location.replace("/games/" + id + "/awaitGame");
-                }
-                })
+          window.location.replace('/games/' + id + '/awaitGame')
         }
+      })
+  }
 
+  const listGames = () => {
+    const token = tokenService.getLocalAccessToken()
 
-    const listGames = () => {
-        
-        const token = localStorage.getItem("jwtToken");
+    const url = apiUrl + '/games/findGames'
 
-        let url = apiUrl + "/games/findGames";
-      
-       
-        axios.get(url,
-            {
-                headers: {
-                "Authorization": `Bearer  ${token}`
-                }
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then(response => {
+        setGames(response.data)
+      })
+  }
 
-            }).then( response =>{
-                setGames(response.data);
-                
-                })
-        }
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className='container'>
+        {games.length === 0 && (
+          <div>
+            <br></br>
 
-    
-    return(
+            <h1>No hay partidas disponibles en este momento</h1>
+          </div>
+        )}
 
-        <React.Fragment>
-        <NavBar></NavBar>
-        <div className="container">
-        {games.length === 0 &&
-            <div>
-                <br></br>
-                
-                <h1>No hay partidas disponibles en este momento</h1>
-
-            </div>
-        }
-
-        {games.length > 0 &&
-        <table className="table table-hover">
-
+        {games.length > 0 && (
+          <table className='table table-hover'>
             <thead>
-                <tr>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Tiempo</th>
-                    <th scope="col">Acciones</th>
-                    
-                </tr>
+              <tr>
+                <th scope='col'>Nombre</th>
+                <th scope='col'>Tiempo</th>
+                <th scope='col'>Acciones</th>
+              </tr>
             </thead>
 
             <tbody>
-                { games.map((value,i) =>{
-                    return (
-                        <tr key={i}>
-                            <td>{value.name} </td>
-                            <td>{value.tiempo} min</td>
-                            
-                           
-                            <td>
-                               {value.numeroJugadores===1?<Button color="primary" onClick={() => unirsePartida(value.id,value.tiempo)}> Unirse</Button>:''}
-                               {' '}
-                               {value.espectadores===true?<Button color="success" onClick={() => espectador(value.id,value.tiempo)}> Espectador</Button>:''}
-                            </td>
-                            
-                        </tr>
-                        
-                    )
-                    
-                })}
+              {games.map((value, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{value.name} </td>
+                    <td>{value.tiempo} min</td>
+                    <td>
+                      {value.numeroJugadores === 1 ? (
+                        <Button
+                          onClick={() => unirsePartida(value.id, value.tiempo)}>
+                          Unirse
+                        </Button>
+                      ) : undefined}
+                      <JoinSpectator
+                        allowSpectators={value.espectadores}
+                        joinSpectator={espectador}
+                        id={value.id}
+                        time={value.time}
+                      />
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
-        </table>
-    }
-        </div>
-
-        </React.Fragment>
-        )
+          </table>
+        )}
+      </div>
+    </>
+  )
 }
 
-export default ListGame;
+function JoinSpectator({ allowSpectators, joinSpectator, id, time }) {
+  return (
+    allowSpectators && (
+      <Button variant='success' onClick={() => joinSpectator(id, time)}>
+        Espectador
+      </Button>
+    )
+  )
+}
+
+export default ListGame
--- frontend/src/components/Player/EditPlayer.js
+++ frontend/src/components/Player/EditPlayer.js
@@ -1,164 +1,179 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import axios from 'axios';
+import logo from '../../logo.svg'
+import './EditPlayer.css'
+import axios from 'axios'
+import NavBar from '../../Navbar'
+import tokenService from '../../services/tokenService'
 
-import './EditPlayer.css';
-
-import logo from "../../logo.svg";
-
-import {envLoader} from '../../env/envLoader';
-
-
-import NavBar from '../../Navbar';
-
-const apiUrl = "http://localhost:8080/api";
+const apiUrl = process.env.REACT_APP_API_URL
 
 function EditPlayer() {
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    telephone: '',
+    user: { username: '', password: '' },
+  })
 
+  const [error, setError] = useState(false)
 
-    const [form,setForm] = useState({
-      "firstName":"",
-      "lastName": "",
-      "telephone": "",
-      "user":{"username":"", "password":""}
-    })
+  const [errorMsg, setErrorMsg] = useState(false)
 
-    const [error,setError] = useState(false);
+  useEffect(() => {
+    player()
+  }, [])
 
-    const [errorMsg,setErrorMsg] = useState(false);
+  const player = () => {
+    const token = tokenService.getLocalAccessToken()
 
+    const url = apiUrl + '/player/data'
 
-    useEffect(() =>{
-       player();
-        
-    },[]);
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then(response => {
+        const player = response.data
 
+        setForm({
+          firstName: player.firstName,
+          lastName: player.lastName,
+          telephone: player.telephone,
+          user: {
+            username: player.user.username,
+            password: player.user.password,
+          },
+        })
+      })
+  }
 
-    const player = () =>{
-        const token = localStorage.getItem("jwtToken");
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
 
-        let url =  apiUrl + "/player/data";
-      
-       
-        axios.get(url,
-            {
-                headers: {
-                "Authorization": `Bearer  ${token}`
-                }
-
-            }).then( response =>{
-                let player = response.data;
-
-                setForm({"firstName": player.firstName,"lastName" : player.lastName,"telephone": player.telephone,"user":{"username": player.user.username,"password": player.user.password}});
-                })
-
-    }
-
-
-    const handleSubmit =(e) =>{
-        e.preventDefault();
-      }
-  
-  
-    const handleChange = async e =>{
-
-        if(e.target.name==='username' || e.target.name==='password'){
-            setForm({...form,user:{...form.user,[e.target.name]: e.target.value}})
-      
-        } else {
-            setForm({...form, [e.target.name] : e.target.value});
-      }
-  
-      }
-
-
-     
-  
-    const handleButton =(e) => {
-
-        const token = localStorage.getItem("jwtToken");
-
-        let url = apiUrl + "/player/update";
-
-        if(validate(form)){
-          axios.post(url,form,
-            {
-                headers: {
-                    "Authorization": `Bearer  ${token}`
-                }
-    
-            })
-          .then( response =>{
-            console.log(response)
-            if(response.status===200){
-
-              if(response.data.jwtToken){
-                localStorage.setItem("jwtToken",response.data.jwtToken);
-              }
-              alert("Se han modificado los datos correctamente");
-              window.location.replace("/games/list");
-              
-            }
-
-        }).catch(error => {
-          setError(true);
-          setErrorMsg("El nombre de usuario ya existe");
-        });
-        
-        }else{
-          setError(true);
-          setErrorMsg("La longitud de los campos debe ser mayor que 1");
-        }
-       
-        
-        
-    }
-
-        return(
-
-            <React.Fragment>
-
-            <NavBar></NavBar>
-
-              <div className="wrapper fadeInDown">
-              <div id="formContent">
-              
-                <div className="fadeIn first">
-                  <img src={logo} width="100px" alt="User Icon" />
-                </div>
-
-                <form onSubmit={handleSubmit}>
-                  <input type="text"  className="fadeIn second" name="firstName" placeholder="Nombre" value={form.firstName} required onChange={handleChange}/>
-                  <input type="text"  className="fadeIn second" name="lastName" placeholder="Apellidos" value={form.lastName} required onChange={handleChange}/>
-                  <input type="text"  className="fadeIn second" name="telephone" placeholder="Telefono" value={form.telephone} required onChange={handleChange}/>
-                  <input type="text"  className="fadeIn second" name="username" placeholder="Usuario" value={form.user.username} required onChange={handleChange}/>
-                  <input type="password" className="fadeIn third" name="password" placeholder="Contraseña" value={form.user.password} required onChange={handleChange}/>
-                  <input type="submit"  value="Actualizar" onClick={handleButton}/>
-                </form>
-
-            {error === true &&
-              <div className="alert alert-danger" role = "alert">
-                {errorMsg}
-              </div>
-            }
-            </div>
-            </div>
-
-
-            </React.Fragment>
-
-
-
-        )
-
-}
-
-function validate(props){
-  if (props["firstName"].length > 1 && props["lastName"].length > 1 && props["telephone"].length > 1){
-    return true;
-    }else{
-      return false;
+  const handleChange = e => {
+    if (e.target.name === 'username' || e.target.name === 'password') {
+      setForm({
+        ...form,
+        user: { ...form.user, [e.target.name]: e.target.value },
+      })
+    } else {
+      setForm({ ...form, [e.target.name]: e.target.value })
     }
   }
 
-export default EditPlayer;
+  const handleButton = e => {
+    const token = tokenService.getLocalAccessToken()
+
+    const url = apiUrl + '/player/update'
+
+    if (validate(form)) {
+      axios
+        .post(url, form, {
+          headers: {
+            Authorization: `Bearer  ${token}`,
+          },
+        })
+        .then(response => {
+          console.log(response)
+          if (response.status === 200) {
+            if (response.data.jwtToken) {
+              localStorage.setItem('jwt', response.data.jwtToken)
+            }
+            alert('Se han modificado los datos correctamente')
+            window.location.replace('/games/list')
+          }
+        })
+        .catch(error => {
+          setError(true)
+          setErrorMsg('El nombre de usuario ya existe')
+          console.log(error)
+        })
+    } else {
+      setError(true)
+      setErrorMsg('La longitud de los campos debe ser mayor que 1')
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <NavBar></NavBar>
+
+      <div className='wrapper fadeInDown'>
+        <div id='formContent'>
+          <div className='fadeIn first'>
+            <img src={logo} width='100px' alt='User Icon' />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              className='fadeIn second'
+              name='firstName'
+              placeholder='Nombre'
+              value={form.firstName}
+              required
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              className='fadeIn second'
+              name='lastName'
+              placeholder='Apellidos'
+              value={form.lastName}
+              required
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              className='fadeIn second'
+              name='telephone'
+              placeholder='Telefono'
+              value={form.telephone}
+              required
+              onChange={handleChange}
+            />
+            <input
+              type='text'
+              className='fadeIn second'
+              name='username'
+              placeholder='Usuario'
+              value={form.user.username}
+              required
+              onChange={handleChange}
+            />
+            <input
+              type='password'
+              className='fadeIn third'
+              name='password'
+              placeholder='Contraseña'
+              value={form.user.password}
+              required
+              onChange={handleChange}
+            />
+            <input type='submit' value='Actualizar' onClick={handleButton} />
+          </form>
+
+          {error === true && (
+            <div className='alert alert-danger' role='alert'>
+              {errorMsg}
+            </div>
+          )}
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+function validate(props) {
+  return (
+    props.firstName.length > 1 &&
+    props.lastName.length > 1 &&
+    props.telephone.length > 1
+  )
+}
+
+export default EditPlayer
