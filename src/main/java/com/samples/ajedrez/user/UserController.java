51e1077b1fbc564dd936fc4aaff6c85164a3dcd1
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-25 12:59:01 +0200

    feat: added check health api

--- src/main/java/com/samples/ajedrez/user/UserController.java
+++ src/main/java/com/samples/ajedrez/user/UserController.java
@@ -3,12 +3,12 @@ package com.samples.ajedrez.user;
 import com.samples.ajedrez.player.Player;
 import com.samples.ajedrez.player.PlayerService;
 import com.samples.ajedrez.service.JwtUtilService;
-import com.samples.ajedrez.service.TokenInfo;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,11 +20,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/api")
+@RestController
 public class UserController {
 
     @Autowired
@@ -49,22 +50,30 @@ public class UserController {
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
     }
-
+    
     @GetMapping("/auth/validate_token/{token}")
-    public String tokenValido(@PathVariable String token) {
-
+    public ResponseEntity<String> tokenValido(@PathVariable String token) {
         try {
             Jwts.parser().setSigningKey(JWT_SECRET_KEY).parseClaimsJws(token);
 
-            return "200";
+            return ResponseEntity.ok(token);
         } catch (JwtException e) {
-            return "401";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).
+                    body(e.getCause().toString());
         }
 
     }
+    
+    @GetMapping("/check")
+    public ResponseEntity<String> checkHealth() {
+        String message = "I am running ✅";
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/json;charset=utf8")
+                .body(message);
+    }
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticate(User user) {
+    public ResponseEntity<String> authenticate(@RequestBody User user) {
 
         try {
 
@@ -74,17 +83,15 @@ public class UserController {
 
             final String jwt = jwtUtilService.generateToken(userDetails);
 
-            TokenInfo tokenInfo = new TokenInfo(jwt);
-
-            return ResponseEntity.ok(tokenInfo.getJwtToken());
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(jwt);
 
         } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.toString());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(Player player) {
+    public ResponseEntity<?> register(@RequestBody Player player) {
 
         User user = player.getUser();
 
