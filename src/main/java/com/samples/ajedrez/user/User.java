6d226e4f6052917875a471716e59f64d41ab325a
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-07-29 15:08:17 +0200

    fix: added join column annotation

--- src/main/java/com/samples/ajedrez/user/User.java
+++ src/main/java/com/samples/ajedrez/user/User.java
@@ -8,10 +8,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -41,8 +41,8 @@ public class User {
     @JsonIgnore
     private List<Authorities> authorities;
     
-    @NotNull
     @ManyToOne(optional = false)
+    @JoinColumn(name="plan_id")
     private Plan plan;
 
 }
